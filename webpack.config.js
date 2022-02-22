587dfb1977acebf390252bbd29e6b32b174b4146
David Plaskett <dcplaskett@gmail.com>
2022-02-22 15:08:54 -0600

    Develpoment complete

--- webpack.config.js
+++ webpack.config.js
@@ -3,13 +3,18 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  mode: 'development',
   entry: {
     index: './src/index.js',
     print: './src/print.js',
   },
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+  },
   plugins: [
     new HtmlWebpackPlugin({
-      title: 'Output Management',
+      title: 'Development',
     }),
   ],
   output: {
